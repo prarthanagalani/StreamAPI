83af8a8dae57b7ece4001665abe6f4d146cdd950
prarthanagalani <prarthanagalani@gmail.com>
2024-01-18 19:59:39 +0530

    added uc2.4

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package StreamAPI;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -18,6 +19,11 @@ public class Main {
         //usecase2
         System.out.println("Usecase2: ");
         List<Integer>data = arr.stream().map(n -> n*2).collect(Collectors.toList());
+        System.out.println(data);
+
+        //usecase4
+        System.out.println("Usecase4: ");
+        data = arr.stream().filter(n -> n%2==0).collect(Collectors.toList());
         System.out.println(data);
     }
     
