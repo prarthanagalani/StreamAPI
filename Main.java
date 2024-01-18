0e59fefecb352502fa21f1c09f31b1e9f320cd35
prarthanagalani <prarthanagalani@gmail.com>
2024-01-18 19:43:17 +0530

    added uc2.1

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 package StreamAPI;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void main(String[] args)
+    {
+        ArrayList<Integer>arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+
+        arr.stream().forEach(n -> System.out.println(n));
+    }
     
 }
