06a555a3b1d5f1dde26489771a99e2f593ea77ed
prarthanagalani <prarthanagalani@gmail.com>
2024-01-18 19:48:48 +0530

    added uc2.2

--- Main.java
+++ Main.java
@@ -12,6 +12,10 @@ public class Main {
         arr.add(3);
 
         arr.stream().forEach(n -> System.out.println(n));
+
+        //usecase2
+        System.out.println("Usecase2: ");
+        arr.stream().map(n -> n*2).forEach(n -> System.out.println(n));
     }
     
 }
